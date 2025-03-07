1db21cf8aa9c80af57e45df2e32ef39921f2a700
Jinyoung <glitt5384@naver.com>
2025-03-08 03:38:42 +0900

    initial commit

---
+++ src/main/java/com/redis/archive/RedisArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.redis.archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis/archive/RedisArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
